6010ab9a51d9b95baf8bbbace3e69ceefec71cf1
emin erdal <emnrdl@gmail.com>
2021-08-30 13:20:30 +0300

    ---

--- panel_ui.py
+++ panel_ui.py
@@ -2,23 +2,30 @@ import bpy
 from bpy.types import Operator, Panel
 
 
-class Tool_Panel(Panel):
-    bl_idname = 'OBJECT_PT_eToolSet'
-    bl_label = 'ComicBook Tool Set'
+class Access_Tool_Panel(Panel):
+    bl_idname = 'ACCESS_OBJECT_PT_eToolSet'
+    bl_label = 'Access'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'EMNrdl Tool Set'
 
     def draw(self, context):
         layout = self.layout
-        placeholder = bpy.context.scene
-
-        layout.label(text="Access:")
 
         row = layout.row()
         row.operator('open.workdir',text='Opens the work directory',icon='HOME')      
 
-        layout.label(text="Selection:")
+
+class Selection_Tool_Panel(Panel):
+    bl_idname = 'SELECT_OBJECT_PT_eToolSet'
+    bl_label = 'Selection'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
 
         row = layout.row()
         row.operator('select.meshs',text='Select Meshs',icon='RESTRICT_SELECT_OFF')
@@ -41,7 +48,17 @@ class Tool_Panel(Panel):
         row = layout.row()
         row.operator('select.cacheable',text='Select Cacheable',icon='RESTRICT_SELECT_OFF')
 
-        layout.label(text="Editing:")
+
+class Editing_Tool_Panel(Panel):
+    bl_idname = 'EDITING_OBJECT_PT_eToolSet'
+    bl_label = 'Editing'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
 
         row = layout.row()
         row.operator('copy.objects',text='Copy to Objects',icon='CON_TRACKTO')
@@ -52,10 +69,22 @@ class Tool_Panel(Panel):
         row = layout.row()
         row.operator('add.bbox',text='Create BBOX from Selected',icon='CUBE')
 
-        layout.label(text="Render:")
+class Render_Tool_Panel(Panel):
+    bl_idname = 'RENDER_OBJECT_PT_eToolSet'
+    bl_label = 'Render'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
 
         row = layout.row()
         row.operator('set.linerender',text='Set Render Settings',icon='RESTRICT_RENDER_OFF')
+
+        row = layout.row()
+        row.operator('create.cameratoview',text='Create Camera from view',icon='RESTRICT_SELECT_OFF')
 
         row = layout.row()
         row.operator('set.cameraactive',text='Set Active Camera',icon='OUTLINER_OB_CAMERA')
@@ -63,7 +92,17 @@ class Tool_Panel(Panel):
         row = layout.row()
         row.operator('create.outline',text='Create GP Outline',icon='OUTLINER_OB_GREASEPENCIL')
 
-        layout.label(text="Property:")
+
+class Property_Tool_Panel(Panel):
+    bl_idname = 'PROPERTY_OBJECT_PT_eToolSet'
+    bl_label = 'Property'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
 
         row = layout.row()
         row.operator('add.camres',text='Add Resolution to Camera',icon='OUTLINER_OB_CAMERA')
@@ -71,7 +110,16 @@ class Tool_Panel(Panel):
         row = layout.row()
         row.operator('add.cacheable',text='Add Cacheable Property',icon='RNA_ADD')
 
-        layout.label(text="Export:")
+class Export_Tool_Panel(Panel):
+    bl_idname = 'EXPORT_OBJECT_PT_eToolSet'
+    bl_label = 'Export'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
 
         row = layout.row()
         row.operator('export.camera',text='Export Cameras',icon='CON_CAMERASOLVER')
@@ -79,3 +127,19 @@ class Tool_Panel(Panel):
         row = layout.row()
         row.operator('export.cacheable',text='Export Cacheable',icon='RNA_ADD')
 
+class Test_Tool_Panel(Panel):
+    bl_idname = 'TEST_OBJECT_PT_eToolSet'
+    bl_label = 'Export'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'EMNrdl Tool Set'
+    
+    def draw(self, context):
+
+        layout = self.layout
+
+        scn = context.scene
+        col = layout.column(align=True)
+        col.prop(scn.my_tool, "path", text="")
+
+        print(scn.my_tool.path)
